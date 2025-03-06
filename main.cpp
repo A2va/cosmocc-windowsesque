c8798ae8704a0bc60dda9af6909f039b4166ec61
A2va <49582555+A2va@users.noreply.github.com>
2025-03-06 12:51:10 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <windowsesque.h>
+#include <sys/utsname.h>
+
+#define GetModuleFileNameW GetModuleFileName
+
+int main(int argc, char** argv) {
+    char16_t buf[2048] = {0};
+    uint32_t size = GetModuleFileNameW(0, buf, (DWORD)2048);
+    return 0;
+}
